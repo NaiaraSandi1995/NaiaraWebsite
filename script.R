34a11af94b8b82b3e833157d7eac7ef50ea5f1ff
Naiara Sandi de Almeida Alcantara <nayara_sandy@hotmail.com>
2023-09-28 22:47:56 -0300

    Update

---
+++ script.R
@@ -0,0 +1,16 @@
+
+blogdown::install_hugo(force = T)
+library(blogdown)
+serve_site()
+blogdown::build_site()
+#Terminarl 
+# 
+
+# usethis::use_git()
+# 3
+# usethis::use_github()
+
+
+# git add .
+# git commit -m "Update"
+# git push 
